21ee6eeeedb8b018336fca0c272332c8a43ec7c7
github username <github email address>
2020-05-04 15:53:23 -0400

    changed user#create to user_find_or_create_by & uncommented cors gem

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -11,15 +11,18 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
-      if user.save
-        render json: user
-      else
-        render error: {error: ‘Unable to create User.’}, status:400
-      end
+    # find_or_create_by
+    user = User.find_or_create_by(username: user_params[:username])
+    # user = User.new(user_params)
+    #   if user.save
+    #     render json: user
+    #   else
+    #     render error: {error: ‘Unable to create User.’}, status:400
+    #   end
   end
 
   def update
+    
     user = User.find(params[:id])
       if user
         user.update(user_params)
@@ -30,6 +33,7 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def destroy
+    # don't need a destroy user method
     user = User.find(params[:id])
     if user
       user.destroy
