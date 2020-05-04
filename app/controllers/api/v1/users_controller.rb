db37afc8382353ec518677977d8754f9c300b71a
github username <github email address>
2020-05-04 17:23:59 -0400

    fixed cors issue

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -13,35 +13,36 @@ class Api::V1::UsersController < ApplicationController
   def create
     # find_or_create_by
     user = User.find_or_create_by(username: user_params[:username])
+    render json: user
     # user = User.new(user_params)
-    #   if user.save
-    #     render json: user
-    #   else
-    #     render error: {error: ‘Unable to create User.’}, status:400
-    #   end
+      # if user.save
+      #   render json: user
+      # else
+      #   render error: {error: ‘Unable to create User.’}, status:400
+      # end
   end
 
-  def update
+  # def update
     
-    user = User.find(params[:id])
-      if user
-        user.update(user_params)
-        render json: {message: ‘User successfully updated’}, status: 200
-      else
-        render json: {error: ‘Unable to update User’}, status:400
-      end
-  end
+  #   user = User.find(params[:id])
+  #     if user
+  #       user.update(user_params)
+  #       render json: {message: ‘User successfully updated’}, status: 200
+  #     else
+  #       render json: {error: ‘Unable to update User’}, status:400
+  #     end
+  # end
 
-  def destroy
-    # don't need a destroy user method
-    user = User.find(params[:id])
-    if user
-      user.destroy
-      render json: {message: ‘User successfully deleted’}, status: 200
-    else
-      render json: {error: ‘Unable to delete User’}, status: 400
-    end
-  end
+  # def destroy
+  #   # don't need a destroy user method
+  #   user = User.find(params[:id])
+  #   if user
+  #     user.destroy
+  #     render json: {message: ‘User successfully deleted’}, status: 200
+  #   else
+  #     render json: {error: ‘Unable to delete User’}, status: 400
+  #   end
+  # end
 
   private
 
